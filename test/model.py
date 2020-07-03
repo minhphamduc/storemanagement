edc740d09bc7fde74781fc4afce7661b4addfa6b
Minh Pham Duc <phamminh06051994@gmail.com>
2020-07-03 10:36:08 +0700

    Mod: Tổng quát hoá kết nối đến mongoDB ở thư mục test Mod: Đổi tên database mongoDB ở thư mục test

--- test/model.py
+++ test/model.py
@@ -2,6 +2,6 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from umongo import Instance
 
 # Connect to Mongodb
-MONGO_CLIENT = AsyncIOMotorClient('mongodb://root:123456@192.168.99.100')
-database = MONGO_CLIENT['users']
+MONGO_CLIENT = AsyncIOMotorClient('mongodb://user:password@ip_address')
+database = MONGO_CLIENT['test_database']
 instance = Instance(database)
