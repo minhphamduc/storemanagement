1af68900bb19c6eb0e903da1a06cbc9cc4342def
minhphamduc <phamminh06051994@gmail.com>
2021-01-27 11:15:57 +0700

    Mod: Khởi tạo app starlette

--- graphql_api/app.py
+++ graphql_api/app.py
@@ -2,3 +2,7 @@ from starlette.applications import Starlette
 from ariadne.asgi import GraphQL
 # Import schema executable
 from graphql_api.combines.schema import schema
+
+
+app = Starlette(debug=True)
+app.mount("/graphql", GraphQL(schema, debug=True))
