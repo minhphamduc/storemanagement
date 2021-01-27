b0930b95e8e15ea7ebfd2d9ddcf419cfe0ecf161
minhphamduc <phamminh06051994@gmail.com>
2021-01-27 14:59:14 +0700

    Mod: Sắp xếp lại query

--- graphql_api/resources/query/resolvers.py
+++ graphql_api/resources/query/resolvers.py
@@ -16,6 +16,74 @@ from graphql_api.resources.resource.models import Resource
 query = ObjectType("Query")
 
 
+@query.field("resource")
+async def resolve_resource(obj: Any, info: GraphQLResolveInfo, **kwargs) \
+        -> Optional[Resource]:
+    """
+    Hàm resolve resource
+    Tham số query:
+        - resource_id: ObjectID dạng string
+
+    :param obj:
+    :param info:
+    :param kwargs:
+    :return:
+    """
+
+    if 'resource_id' in kwargs:
+        return await Resource.find_one({'_id': ObjectId(
+            kwargs['resource_id']
+        )})
+
+    else:
+        return None
+
+
+@query.field("tenant")
+async def resolve_tenant(obj: Any, info: GraphQLResolveInfo, **kwargs) \
+        -> Optional[Tenant]:
+    """
+    Hàm resolve tenant
+    Tham số query:
+        - tenant_id: ObjectID dạng string
+
+    :param obj:
+    :param info:
+    :param kwargs:
+    :return:
+    """
+
+    if 'tenant_id' in kwargs:
+        return await Tenant.find_one({'_id': ObjectId(kwargs['tenant_id'])})
+
+    else:
+        return None
+
+
+@query.field("tenant_setting")
+async def resolve_tenant_setting(obj: Any, info: GraphQLResolveInfo,
+                                 **kwargs) \
+        -> Optional[TenantSetting]:
+    """
+    Hàm resolve tenant setting
+    Tham số query:
+        - tenant_setting_id: ObjectID dạng string
+
+    :param obj:
+    :param info:
+    :param kwargs:
+    :return:
+    """
+
+    if 'tenant_setting_id' in kwargs:
+        return await TenantSetting.find_one(
+            {'_id': ObjectId(kwargs['tenant_setting_id'])}
+        )
+
+    else:
+        return None
+
+
 @query.field("plan")
 async def resolve_plan(obj: Any, info: GraphQLResolveInfo, **kwargs) \
         -> Optional[Plan]:
@@ -32,6 +100,29 @@ async def resolve_plan(obj: Any, info: GraphQLResolveInfo, **kwargs) \
 
     if 'plan_id' in kwargs:
         return await Plan.find_one({'_id': ObjectId(kwargs['plan_id'])})
+
+    else:
+        return None
+
+
+@query.field("tenant_plan")
+async def resolve_tenant_plan(obj: Any, info: GraphQLResolveInfo, **kwargs) \
+        -> Optional[TenantPlan]:
+    """
+    Hàm resolve tenant plan
+    Tham số query:
+        - tenant_plan_id: ObjectID dạng string
+
+    :param obj:
+    :param info:
+    :param kwargs:
+    :return:
+    """
+
+    if 'tenant_plan_id' in kwargs:
+        return await TenantPlan.find_one({'_id': ObjectId(
+            kwargs['tenant_plan_id']
+        )})
 
     else:
         return None
@@ -96,97 +187,6 @@ async def resolve_user_setting(obj: Any, info: GraphQLResolveInfo, **kwargs) \
     if 'user_setting_id' in kwargs:
         return await UserSetting.find_one({'_id': ObjectId(
             kwargs['user_setting_id']
-        )})
-
-    else:
-        return None
-
-
-@query.field("tenant")
-async def resolve_tenant(obj: Any, info: GraphQLResolveInfo, **kwargs) \
-        -> Optional[Tenant]:
-    """
-    Hàm resolve tenant
-    Tham số query:
-        - tenant_id: ObjectID dạng string
-
-    :param obj:
-    :param info:
-    :param kwargs:
-    :return:
-    """
-
-    if 'tenant_id' in kwargs:
-        return await Tenant.find_one({'_id': ObjectId(kwargs['tenant_id'])})
-
-    else:
-        return None
-
-
-@query.field("tenant_setting")
-async def resolve_tenant_setting(obj: Any, info: GraphQLResolveInfo,
-                                 **kwargs) \
-        -> Optional[TenantSetting]:
-    """
-    Hàm resolve tenant setting
-    Tham số query:
-        - tenant_setting_id: ObjectID dạng string
-
-    :param obj:
-    :param info:
-    :param kwargs:
-    :return:
-    """
-
-    if 'tenant_setting_id' in kwargs:
-        return await TenantSetting.find_one(
-            {'_id': ObjectId(kwargs['tenant_setting_id'])}
-        )
-
-    else:
-        return None
-
-
-@query.field("tenant_plan")
-async def resolve_tenant_plan(obj: Any, info: GraphQLResolveInfo, **kwargs) \
-        -> Optional[TenantPlan]:
-    """
-    Hàm resolve tenant plan
-    Tham số query:
-        - tenant_plan_id: ObjectID dạng string
-
-    :param obj:
-    :param info:
-    :param kwargs:
-    :return:
-    """
-
-    if 'tenant_plan_id' in kwargs:
-        return await TenantPlan.find_one({'_id': ObjectId(
-            kwargs['tenant_plan_id']
-        )})
-
-    else:
-        return None
-
-
-@query.field("resource")
-async def resolve_resource(obj: Any, info: GraphQLResolveInfo, **kwargs) \
-        -> Optional[Resource]:
-    """
-    Hàm resolve resource
-    Tham số query:
-        - resource_id: ObjectID dạng string
-
-    :param obj:
-    :param info:
-    :param kwargs:
-    :return:
-    """
-
-    if 'resource_id' in kwargs:
-        return await Resource.find_one({'_id': ObjectId(
-            kwargs['resource_id']
         )})
 
     else:
